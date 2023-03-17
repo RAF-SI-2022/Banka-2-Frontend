439250aef82a7876c273363bdf14a4c3584f1091
SrdjanLazic <slazic3219rn@raf.rs>
2023-03-17 13:15:34 +0100

    Small changes, preparation for sprint2 branch

---
+++ src/app/models/stock-exchange.model.ts
@@ -0,0 +1,36 @@
+export interface Currency {
+  currencyName: string,
+  currencyCode: string,
+  currencySymbol: string,
+  polity: string
+}
+
+export interface Exchange {
+  exchangeName: string,
+  exchangeAcronym: string,
+  exchangeMICCode: string,
+  polity: string,
+  currency: Currency,
+  timeZone: number
+}
+
+export interface Listing {
+  ticker: string,
+  name: string,
+  exchange: Exchange,
+  lastRefresh: Date,
+  price: number,
+  ask: number,
+  bid: number,
+  change: number,
+  volume: number
+}
+
+export interface Stock {
+  outstandingShares: number,
+  dividendYield: number
+}
+
+
+
+

e903bb5daa3187046e6e8737ea6640d96d9f23a9
dimitrijevich <107809349+dimitrijevich@users.noreply.github.com>
2023-04-04 00:29:24 +0200

    dodata direktiva za validaciju

---
+++ src/app/directives/allow-numbers-only.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appAllowNumbersOnly]'
+})
+export class AllowNumbersOnlyDirective {
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent): void {
+    const allowedKeys: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Backspace', 'Tab', 'ArrowLeft', 'ArrowRight'];
+
+    if (allowedKeys.indexOf(event.key) === -1) {
+      event.preventDefault();
+    }
+  }
+}

4b2e8903549844f087fde0d4d948d383ede39ce8
Luka Jeremic <ljeremic4019rn@raf.rs>
2023-06-06 04:21:41 +0200

    test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,4 @@
 export const environment = {
-
-  // mainHost: 'localhost',
-  // otcHost: 'localhost',
-  // usersHost: 'localhost',
-  // mainPort: '8080',
-  // usersPort: '8081',
-  // otcPort: '8082',
-
   mainServiceURL: 'http://localhost:8080',
   // mainURL: host+port
   usersServiceURL: 'http://localhost:8081',
@@ -15,40 +7,8 @@ export const environment = {
   // otcURL: host+port
 
 
-  // 80-main
-  // 81-users
-  // 82-otc
-
-  // //mainService postojece putanje
-  // apiServiceUrl: 'http://localhost:8080/api',
-  // apiAuthServerUrl: 'http://localhost:8080/auth',
-  // apiUserServiceUrl: 'http://localhost:8080/api/users',
-  // apiYahooFinanceUrl: 'https://query1.finance.yahoo.com/v11/finance/quoteSummary/',
-  // apiStockServiceUrl: 'http://localhost:8080/api/stock',
-  // apiForexServiceUrl: 'http://localhost:8080/api/forex',
-  // apiExchangeServiceUrl: 'http://localhost:8080/api/exchange',
-  // apiFutureServiceUrl: 'http://localhost:8080/api/futures',
-  // apiOptionsServiceUrl: 'http://localhost:8080/api/options',
-
-  //usersService postojece putanje
-
-
-  //otcService postojece putanje
-
 
   //utility putanje
   csvPath: 'assets/csv/filtered_forex_pairs.csv'
 
-  /*
-  apiAuthServerUrl: 'http://localhost:8080/auth',
-  apiUserServiceUrl: 'http://localhost:8080/api/users',
-  apiForgotPassUrl: 'http://localhost:8080/auth/reset-password',
-  apiPasswordRestoreUrl: 'http://localhost:8080/auth/change-user-password',
-  apiForexUrl: 'http://localhost:8080/api/forex',
-  apiYahooFinanceUrl: 'https://query1.finance.yahoo.com/v11/finance/quoteSummary/',
-  apiStockServiceUrl: 'http://localhost:8080/api/stock',
-  apiStockDetails: 'http://localhost:8080/api/stock/',
-  apiStockGraph: `http://localhost:8080/api/stock/`,
-  apiExchangeServiceUrl: 'http://localhost:8080/api/exchange'
-   */
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,4 @@
 export const environment = {
-  // mainHost: 'localhost',
-  // otcHost: 'localhost',
-  // usersHost: 'localhost',
-  // mainPort: '8080',
-  // usersPort: '8081',
-  // otcPort: '8082',
 
   mainServiceURL: 'http://localhost:8080',
   // mainURL: host+port
@@ -12,28 +6,6 @@ export const environment = {
   // usersURL: host+port
   otcServiceURL: 'http://localhost:8082',
   // otcURL: host+port
-
-
-  // 80-main
-  // 81-users
-  // 82-otc
-
-  // //mainService postojece putanje
-  // apiServiceUrl: 'http://localhost:8080/api',
-  // apiAuthServerUrl: 'http://localhost:8080/auth',
-  // apiUserServiceUrl: 'http://localhost:8080/api/users',
-  // apiYahooFinanceUrl: 'https://query1.finance.yahoo.com/v11/finance/quoteSummary/',
-  // apiStockServiceUrl: 'http://localhost:8080/api/stock',
-  // apiForexServiceUrl: 'http://localhost:8080/api/forex',
-  // apiExchangeServiceUrl: 'http://localhost:8080/api/exchange',
-  // apiFutureServiceUrl: 'http://localhost:8080/api/futures',
-  // apiOptionsServiceUrl: 'http://localhost:8080/api/options',
-
-  //usersService postojece putanje
-
-
-  //otcService postojece putanje
-
 
   //utility putanje
   csvPath: 'assets/csv/filtered_forex_pairs.csv'

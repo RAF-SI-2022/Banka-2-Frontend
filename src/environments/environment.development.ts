c5025fc7c7c38a6f86591589e21b95062f068f0b
Luka Jeremic <ljeremic4019rn@raf.rs>
2023-06-09 12:44:32 +0200

    IZMENJENI PORTOVI DA RADE

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  mainServiceURL: 'http://localhost:8080',
+  usersServiceURL: 'http://localhost:8080',
   // mainURL: host+port
-  usersServiceURL: 'http://localhost:8081',
+  mainServiceURL: 'http://localhost:8081',
   // usersURL: host+port
   otcServiceURL: 'http://localhost:8082',
   // otcURL: host+port
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
 
-  mainServiceURL: 'https://banka-2.k8s.elab.rs/backend/main',
-  // mainURL: host+port
   usersServiceURL: 'https://banka-2.k8s.elab.rs/backend/users',
+  // mainURL: host+port
+  mainServiceURL: 'https://banka-2.k8s.elab.rs/backend/main',
   // usersURL: host+port
   otcServiceURL: 'https://banka-2.k8s.elab.rs/backend/otc',
   // otcURL: host+port

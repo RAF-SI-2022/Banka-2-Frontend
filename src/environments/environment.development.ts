8b7e60f6b6d72f4e73bc57f09c2398d56a72ccf4
dimitrijevich <dimitrijevicraf@gmail.com>
2023-06-10 18:38:42 +0200

    placanja

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,14 +1,14 @@
 export const environment = {
-  mainServiceURL: 'http://localhost:8080',
+  usersServiceURL: 'http://localhost:8080',
   // mainURL: host+port
-  usersServiceURL: 'http://localhost:8081',
+  mainServiceURL: 'http://localhost:8081',
   // usersURL: host+port
   otcServiceURL: 'http://localhost:8082',
   // otcURL: host+port
+
 
 
   //utility putanje
   csvPath: 'assets/csv/filtered_forex_pairs.csv'
 
 };
-

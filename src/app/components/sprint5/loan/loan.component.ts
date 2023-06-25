11146b60c9f79a8175623cfbae13fef9c3aa4c60
lradovanovic8919rn <lradovanovic8919rn@raf.rs>
2023-06-25 22:29:03 +0200

    placanje rate kredita

--- src/app/components/sprint5/loan/loan.component.ts
+++ src/app/components/sprint5/loan/loan.component.ts
@@ -114,6 +114,18 @@ export class LoanComponent {
 
   }
 
+  payRate(loanId:string){
+    this.clientService.payRate(loanId).subscribe({
+      next: value => {
+        console.log("RATA PLACENA")
+
+      },
+      error: err => {
+        console.log("DOSLO JE DO GRESKE PRILIKOM PLACANJA RATE")
+      }
+    })
+  }
+
   openRequestLoanDialog() {
     this.requestLoanComponent.open();
   }
